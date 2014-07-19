e70e5d5848dcd90d1ead8f34b51fb4a947f5c456
Bartosz Krawczyk <bartoszkrawczyk@Mac-mini-Bartosz.local>
2014-07-20 01:05:29 +0200

    backend update

--- asg_server.php
+++ asg_server.php
@@ -28,42 +28,34 @@
 
 	header('Content-Type: application/json');
 	
-	$DB_LINK = mysqli_connect($asg_DbHost, $asg_DbUser, $asg_DbPass, $asg_DbName);
-	mysqli_query ($DB_LINK, "SET NAMES 'utf8'");
+	$csi = new mysqli($asg_DbHost, $asg_DbUser, $asg_DbPass, $asg_DbName);
+	$csi->query(' SET NAMES utf8');
 
-
-	if ((isset ($_POST ['query'])) && (trim ($_POST ['query']) != ''))
-	{
+	if ((isset ($_POST ['query'])) && (trim ($_POST ['query']) != '')) {
 		$return = array();
-		$query = mysqli_real_escape_string ($DB_LINK, trim ($_POST ['query']));
-		$result = mysqli_query ($DB_LINK, "SELECT * FROM `$asg_TableName` WHERE `$asg_SearchColumn` LIKE '%{$query}%'");			
-		if (mysqli_num_rows ($result) > 0)
-		{
+		$query = $csi->real_escape_string(trim ($_POST ['query']));
+		$result = $csi->query("SELECT * FROM `$asg_TableName` WHERE `$asg_SearchColumn` LIKE '%{$query}%'");
+		if ($result->num_rows > 0) {
 			$count = 1;
 			
-			while ($row = mysqli_fetch_assoc ($result))
-			{
+			while ($row = $result->fetch_assoc()) {
 				$return[] = array(
 					'id' => $row [$asg_resultIdColumn],
 					'name' => $row [$asg_SearchColumn],
-					'count' => $count
+					'count' => $count,
+					'error' => 0
 				);
 				$count++;
 			}
-		}
-		else
-		{
-			$returnt[] = array(
+		} else {
+			$return[] = array(
 				'id' => 0,
-				'name' => $asg_notFoundText
+				'name' => $asg_notFoundText,
+				'count' => 0,
+				'error' => 1
 			);
 		}
 	}
 	
 	echo json_encode($return);
-	
 ?>
-
-
-
-
--- jquery.autosuggest.js
+++ jquery.autosuggest.js
@@ -43,40 +43,45 @@ $.fn.autosuggest = function (options) {
 		input.attr('data-id', '0');
 		$('.cp-asuggest-list-box').empty();
 		var selected = 0;
-    	var list_elems = 0;
-    	if (input.val().trim().length >= start)
-    	{
-    		timer = 0;
+		var list_elems = 0;
+		if (input.val().trim().length >= start)
+		{
+			timer = 0;
 			function asgSearch (){ 
 				selected = 0;
-			    var qr = input.val().trim();
-			    if (input.val().trim().length > start)
-    			{
-    				input.next('.cp-asuggest-list-box').empty();
-	    			$.ajax({
-	    				url: settings.server,
-	    				method: 'POST',
-	    				dataType: 'json',
-	    				data: { query: qr },
-	    				success: function(response) {
-	    					$.each(response, function(index, value) {
-	    						input.next('.cp-asuggest-list-box').append('<div data-id="'+value.id+'" class="cp-asuggest-list cp-asg-onlist-'+value.count+'">'+value.name+'</div>')
-	    					});
-	    					list_elems = input.closest('.cp-asuggest-wrap').find('.cp-asuggest-list').length;
+				var qr = input.val().trim();
+				if (input.val().trim().length > start)
+				{
+					input.next('.cp-asuggest-list-box').empty();
+					$.ajax({
+						url: settings.server,
+						method: 'POST',
+						dataType: 'json',
+						data: { query: qr },
+						success: function(response) {
+							$.each(response, function(index, value) {
+								if (value.error == 1) {
+									var err_class = ' cp-asuggest-list-error';
+								} else {
+									var err_class = '';
+								}
+								input.next('.cp-asuggest-list-box').append('<div data-id="'+value.id+'" class="cp-asuggest-list'+err_class+' cp-asg-onlist-'+value.count+'">'+value.name+'</div>')
+							});
+							list_elems = input.closest('.cp-asuggest-wrap').find('.cp-asuggest-list').length;
 							input.closest('.cp-asuggest-wrap').find('.cp-asuggest-list').highlight(qr);
-	    				}
-	    			});
-    			} else {
-    				input.next('.cp-asuggest-list-box').empty();
-    			}
+						}
+					});
+				} else {
+					input.next('.cp-asuggest-list-box').empty();
+				}
 			}
 			input.off('keyup').on('keyup', function(e){
 				input.attr('data-id', '0');
 				if ((e.which != 38) && (e.which != 40) && (e.which != 13)) {
-				    if (timer) {
-				        clearTimeout(timer);
-				    }
-				    timer = setTimeout(asgSearch, settings.delay);
+					if (timer) {
+						clearTimeout(timer);
+					}
+					timer = setTimeout(asgSearch, settings.delay);
 				} else {
 					if (e.which == 40) {
 						if (selected == list_elems) {
@@ -104,32 +109,32 @@ $.fn.autosuggest = function (options) {
 					if (e.which == 13) {
 						if (selected > 0) {
 							var text = input.closest('.cp-asuggest-wrap').find('.cp-asg-onlist-'+selected).text();
-    						var result_id = input.closest('.cp-asuggest-wrap').find('.cp-asg-onlist-'+selected).attr('data-id');
+							var result_id = input.closest('.cp-asuggest-wrap').find('.cp-asg-onlist-'+selected).attr('data-id');
 							input.next('.cp-asuggest-list-box').empty();
-    						input.val(text).attr('data-id', result_id);
+							input.val(text).attr('data-id', result_id);
 						}
 					}
 				}
 			});
-    	}
+		}
 	});
 	
 	$(document).off('click', '.cp-asuggest-list').on('click', '.cp-asuggest-list', function (e) {
-    	var text = $(this).text(),
+		var text = $(this).text(),
 			result_id = $(this).attr('data-id');
-    	
-    	if (result_id != '0') {
-    		$(this).closest('.cp-asuggest-wrap').find('input').val(text).attr('data-id', result_id);
-    		$(this).closest('.cp-asuggest-list-box').empty();
-    	}
-    	
-    	e.preventDefault();
-    	return false;
-    });
-    
-    $(document).on('click', 'html', function (e) {
-    	$('.cp-asuggest-list-box').empty();
-    });
+		
+		if (result_id != '0') {
+			$(this).closest('.cp-asuggest-wrap').find('input').val(text).attr('data-id', result_id);
+			$(this).closest('.cp-asuggest-list-box').empty();
+		}
+		
+		e.preventDefault();
+		return false;
+	});
+	
+	$(document).on('click', 'html', function (e) {
+		$('.cp-asuggest-list-box').empty();
+	});
 };
 
 
